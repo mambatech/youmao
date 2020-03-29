2854f4acdd628c6716dabfea72ed060aaa5c1d35
mapleyuan <yuanweinan00@gmail.com>
2020-03-29 22:12:06 +0800

    remove unused

--- lib/redux/play/reducer.dart
+++ lib/redux/play/reducer.dart
@@ -31,33 +31,23 @@ GlobalPlayState playStateReducer(GlobalPlayState state, action) {
     if( type == Actions.nextSong || type == Actions.prevSong || type == Actions.addPlayList) {
       var co =  action['payload']['coverMainColor'];
       state.coverMainColor = co;
-      print('william-----------------------> action $co');
     }
     if(action['type'] == Actions.addPlayList) {
-      print('william-----------------------> action play');
       if (state.playing) {
-        print('william-----------------------> action playing');
         state.audioPlayer.stop();
         state.playing = false;
       }
       if (action['payload']['songList'] != null) {
-        print('william-----------------------> action ]songList != null');
         state.songList = action['payload']['songList'];
       }
       if (action['payload']['songDetail']['songLyr']['lrc'] != null && action['payload']['songDetail']['songLyr']['lrc']['lyric'] != null) {
-        print('william-----------------------> action ]songsssssssfffff-------33434List != null');
         action['payload']['songDetail']['lyric'] = combinLyric(action['payload']['songDetail']['songLyr']['lrc']['lyric']);
       }
       state.songIndex = action['payload']['songIndex'];
       state.playList.add(action['payload']['songDetail']);
       state.currentIndex = state.playList.length - 1;
       state.songUrl = action['payload']['songUrl'];
-      var songurl = state.songUrl;
-      print('william-----------------------> action play $songurl');
-      if(state.audioPlayer == null ) {
-        print('william-----------------------> action state.audioPlayer == null');
-      }
-      state.audioPlayer.play(songurl);
+      state.audioPlayer.play(state.songUrl);
       state.playing = true;
     }
     else {
