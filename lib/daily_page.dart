8b5779e00e2fe0f34b7b3f76ef8b93a936a5fe1c
liaoshouwang <Edison.liao@cootek.cn>
2020-04-19 14:23:55 +0800

    导入猫咪百科数据

---
+++ lib/daily_page.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/material.dart';
+
+class DailyPage extends StatefulWidget{
+
+
+  @override
+  State<StatefulWidget> createState() {
+    return DailyPageState();
+  }
+
+}
+
+class DailyPageState extends State<DailyPage>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+
+      ],
+    );
+  }
+
+}
